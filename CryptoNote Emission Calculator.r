0f064651376bf6f3a2070142a7770247ad724d5a
Joshua Paul Barnard <joshuapaulbarnard@gmail.com>
2023-04-29 21:02:59 -0700

    Improved Comments
    Added readme.
    Improved Comments.

--- CryptoNote Emission Calculator.r
+++ CryptoNote Emission Calculator.r
@@ -1,35 +1,35 @@
 ################################################################################
 ####   CryptoNote Emissions Calculator   
-####    Author:  Joshua Paul Burnard     
+####    Author:  Joshua Paul Burnard   
+####  
 ####    References:  CryptoNote Emission Calculator    
 ####      https://cryptonotestarter.org/inner.html    
 ####
 ####    Purpose:  To estimate the number of coins which will be mined from the 
 ####      beginning of a CryptoNote-based Coin.
-####    Inputs:   days - Is the number of days to estimate (starting from day 0).
+####  
+####    Inputs:   moneySupply - The total amount of coins to be mined.
+####              difficultyTarget - The ideal time period between blocks. In case an average time between blocks becomes less than difficulty target, difficulty increases. Difficulty target is measured in seconds.
+####              emissionSpeedFactor - There is a slight decrease of block reward each block, which defines emission curve slope. This parameter is required to calulate block reward.
+####              days - Is the number of days to estimate (starting from day 0).
+####  
 ####    Results:  amount_mined  - The total number of coins estimated to be mined.
 ####              percent_mined - The estimated percent of all coins mined.
 ################################################################################
 
 
-
 #############################################
-####   The amount of days to estimate    ####
-#############################################
-days <- 365.25
-
-
-
-#############################################
-####   Parameters                        ####
-####    Default parameters can be changed ###
+####   Input Parameters                  ####
+####                                     ####
 ####    moneySupply must be an integer   ####
 ####    difficultyTarget  is an integer  ####
 ####    emissionSPeedFactor is integer   ####
+####    days is floating-point (numeric) ####
 #############################################
 moneySupply <- 18446744
 difficultyTarget <- 120
 emissionSpeedFactor <- 21
+days <- 365.25
 
 
 
---
+++ README.md
@@ -0,0 +1,27 @@
+# [CryptoNote Emissions Calculator] 
+
+**Purpose:**
+To estimate the number of coins which will be mined from the beginning of a CryptoNote-based Coin.
+
+**Inputs:**
+- [moneySupply] - The total amount of coins to be mined.
+- [difficultyTarget] - The ideal time period between blocks. In case an average time between blocks becomes less than difficulty target, difficulty increases. Difficulty target is measured in seconds.
+- [emissionSpeedFactor] - There is a slight decrease of block reward each block, which defines emission curve slope. This parameter is required to calulate block reward.
+- [days] - Is the number of days to estimate (starting from day 0).
+
+**Results:**
+- [amount_mined]  - The total number of coins estimated to be mined.
+- [percent_mined] - The estimated percent of all coins mined.
+
+________________________________________________________________________________________________________________________________________
+________________________________________________________________________________________________________________________________________
+
+
+**Credits :**
+
+- [CryptoNoteStarter](https://cryptonotestarter.org/inner.html) : Forking guide to create your own anonymous cryptocurrency based on CryptoNote technology and ring signatures.
+
+
+
+**MIT License**
+Copyright (c) 2018 [Joshua Paul Barnard](https://joshuapaulbarnard.github.io/Interactive_Wine_Aroma_Wheel/LICENSE).
