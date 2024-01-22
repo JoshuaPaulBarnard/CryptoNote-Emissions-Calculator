f0218e76c6d526d3960895a4c4874acc95898b63
Joshua Paul Barnard <joshuapaulbarnard@gmail.com>
2024-01-22 10:49:14 -0800

    Added premining
    Added support calculating emissions with Genesis Block Rewards, ie Premining.

--- CryptoNote Emission Calculator.r
+++ CryptoNote Emission Calculator.r
@@ -1,23 +1,27 @@
 ################################################################################
 ####   CryptoNote Emissions Calculator   
 ####    Author:  Joshua Paul Burnard   
+####    https://github.com/JoshuaPaulBarnard/CryptoNote-Emissions-Calculator
+####
 ####  
 ####    References:  CryptoNote Emission Calculator    
 ####      https://cryptonotestarter.org/inner.html#emission   
 ####      https://cryptonotestarter.org/js/tools.js
+####      https://forknote.net/create/
+####      https://github.com/forknote/forknote.net/blob/gh-pages/partials/create.js
 ####
-####    Purpose:  To estimate the number of coins which will be mined from the 
-####      beginning of a CryptoNote-based Coin.
+####
+####    Purpose:  To estimate the number of coins which will be mined from the beginning of a CryptoNote-based Coin.
 ####  
-####    Inputs:   Money_Supply - The total amount of coins to be mined.
-####              Difficulty_Target - The ideal time period between blocks. In case an average time between blocks becomes less than difficulty target, difficulty increases. Difficulty target is measured in seconds.
-####              Emission_Speed_Factor - There is a slight decrease of block reward each block, which defines emission curve slope. This parameter is required to calulate block reward.
-####              Days - Is the number of Days to estimate (starting from day 0).
+####    Inputs:   Money_Supply <- The total amount of coins to be mined.
+####              Premine_Percent <- The percent of coins to be mined in the genesis block.
+####              Difficulty_Target <- The ideal time period between blocks. In case an average time between blocks becomes less than difficulty target, difficulty increases. Difficulty target is measured in seconds.  This is equivalent to average Block Time.
+####              Emission_Speed_Factor <- There is a slight decrease of block reward each block, which defines emission curve slope. This parameter is required to calulate block reward.
+####              Months - Is the number of days to estimate (starting from day 0), with 30.4375 days per month.
 ####  
 ####    Results:  amount_mined  -> The total number of coins estimated to be mined.
 ####              percent_mined -> The estimated percent of all coins mined.
 ####              Estimated_Blocks_Created -> The estimated number of blocks created.  
-####              Average_Block_Time -> The estimated time between the creation of each block (in minutes).
 ################################################################################
 
 
@@ -25,14 +29,17 @@
 ####   Input Parameters                    ####
 ####                                       ####
 ####    Money_Supply must be an integer    ####
+####    Premine_Percent must be an integer ####
 ####    Difficulty_Target  is an integer   ####
 ####    Emission_Speed_Factor is integer   ####
 ####    Months is floating-point (numeric) ####
 ###############################################
-Money_Supply <- 18446744
-Difficulty_Target <- 60
-Emission_Speed_Factor <- 22
+Money_Supply <- 18446744073709551615
+Premine_Percent <- 0
+Difficulty_Target <- 120
+Emission_Speed_Factor <- 21
 Months <- 12
+
 
 
 
@@ -40,13 +47,15 @@ Months <- 12
 ####   The Equations                     ####
 ####    (Do Not Change)                  ####
 #############################################
-Days <- 365.25 * ( Months / 12 )
-Speed_Factor <- 1 / (2 ^ Emission_Speed_Factor)
-amount_mined <- Money_Supply * ( (Speed_Factor - 1) * (1 - Speed_Factor) ^ (Days * 86400 / Difficulty_Target) + 1);
-percent_mined <- ( amount_mined * 100 ) / Money_Supply
-Blocks_Per_Day <- ( 24 * 60 * 60 ) / Difficulty_Target
-Estimated_Blocks_Created <- Blocks_Per_Day * Days
-Average_Block_Time <- Difficulty_Target / 60
+Days                      <- 365.25 * ( Months / 12 )
+Speed_Factor              <- 1 / (2 ^ Emission_Speed_Factor)
+Premined_Amount           <- Money_Supply * Premine_Percent / 100
+amount_mined              <- Premined_Amount + ( Money_Supply - Premined_Amount ) * ( (Speed_Factor - 1) * ( (1 - Speed_Factor) ^ (Days * 86400 / Difficulty_Target) ) + 1 );
+#amount_mined              <- Money_Supply * ( (Speed_Factor - 1) * (1 - Speed_Factor) ^ (Days * 86400 / Difficulty_Target) + 1);
+percent_mined             <- ( amount_mined * 100 ) / Money_Supply
+Blocks_Per_Day            <- ( 24 * 60 * 60 ) / Difficulty_Target
+Estimated_Blocks_Created  <- Blocks_Per_Day * Days
+
 
 
 #############################################
@@ -55,4 +64,3 @@ Average_Block_Time <- Difficulty_Target / 60
 amount_mined
 percent_mined
 Estimated_Blocks_Created
-Average_Block_Time
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CryptoNote Emissions Calculator 
+# CryptoNote Emissions Calculator
 
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) [![No Maintenance Intended](http://unmaintained.tech/badge.svg)](http://unmaintained.tech/)
 
@@ -6,14 +6,16 @@
 To estimate the number of coins which will be mined from the beginning of a CryptoNote-based Coin.
 
 **Inputs:**
-- moneySupply - The total amount of coins to be mined.
-- difficultyTarget - The ideal time period between blocks. In case an average time between blocks becomes less than difficulty target, difficulty increases. Difficulty target is measured in seconds.
-- emissionSpeedFactor - There is a slight decrease of block reward each block, which defines emission curve slope. This parameter is required to calulate block reward.
-- days - Is the number of days to estimate (starting from day 0).
+- Money_Supply <- The total amount of coins to be mined.
+- Premine_Percent <- The percent of coins to be mined in the genesis block.
+- Difficulty_Target <- The ideal time period between blocks. In case an average time between blocks becomes less than difficulty target, difficulty increases. Difficulty target is measured in seconds.  This is equivalent to average Block Time.
+- Emission_Speed_Factor <- There is a slight decrease of block reward each block, which defines emission curve slope. This parameter is required to calculate block reward.
+- Months <- Is the number of days to estimate (starting from day 0), with 30.4375 days per month.
 
 **Results:**
-- amount_mined  - The total number of coins estimated to be mined.
-- percent_mined - The estimated percent of all coins mined.
+- amount_mined  -> The total number of coins estimated to be mined.
+- percent_mined -> The estimated percent of all coins mined.
+- Estimated_Blocks_Created -> The estimated number of blocks created.  
 
 ________________________________________________________________________________________________________________________________________
 ________________________________________________________________________________________________________________________________________
@@ -21,9 +23,10 @@ ________________________________________________________________________________
 
 **Credits :**
 
-- [CryptoNoteStarter](https://cryptonotestarter.org/inner.html) : Forking guide to create your own anonymous cryptocurrency based on CryptoNote technology and ring signatures.
+- [CryptoNoteStarter](https://cryptonotestarter.org/inner.html) : Forking guide to create your own anonymous cryptocurrency based on CryptoNote technology and ring signatures.  It was used to derive the original equations.
+- [Forknote](https://forknote.net/create/) : Forknote is innovative way to create Cryptonote (https://cryptonote.org) based cryptotokens. It gives the users the ability to create cryptotokens just by creating a simple configuration file.  It was used to derive the equations for premining.
 
 
 
 **MIT License**
-Copyright (c) 2023 [Joshua Paul Barnard](https://mit-license.org/).
+Copyright (c) 2023-2024 [Joshua Paul Barnard](https://mit-license.org/).
